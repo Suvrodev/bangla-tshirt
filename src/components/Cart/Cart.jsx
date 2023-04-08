e75bca883e0d034e20aa3026e41a3107d995ad6a
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-04-08 16:37:00 +0600

    Display TShirt and Added Buy Button

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div className='cart_container'>
+            <h1>Order Summery</h1>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Home.css'
 import { Outlet, useLoaderData } from 'react-router-dom';
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
 
 const Home = () => {
     const t_shirts=useLoaderData()
-    console.log(t_shirts)
+  //  console.log(t_shirts)
+
+    const [cart,setCart]=useState([]);
+    const handleAddToCart=tshirt=>{
+        console.log(tshirt)
+    }
+
+
     return (
-        <div>
-            <h2>This is Home</h2>
-            <h1>T-SHirts: {t_shirts.length} </h1>
+        <div className='home_container'>
+            <div className='tshirts_container'>
+                {/* <h2>This is Home</h2>
+                <h1>T-SHirts: {t_shirts.length} </h1> */}
+                {
+                    t_shirts.map(tshirt=> <TShirt
+                    key={tshirt._id}
+                    handleAddToCart={handleAddToCart}
+                    tshirt={tshirt}
+                    ></TShirt> )
+                }
+            </div>
+            <div className="cart_container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
